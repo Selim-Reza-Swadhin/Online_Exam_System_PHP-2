42616f6664bef5b1d148d412b9204c3b03347475
Selim Reza Swadhin <selimrezaswadhin@gmail.com>
2020-10-03 21:00:26 +0600

    Online Exam System PHP 2

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "online_exam_system_php-2");
---
+++ inc/footer.php
@@ -0,0 +1,7 @@
+ </section>
+<section class="footeroption">
+    <h2>&copy; 2017 - <?= date('Y'). " www.projectnotes.org"; ?></h2>
+	</section>
+</div>
+</body>
+</html>
